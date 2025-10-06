cf928cf1dbc8fb390d9e8f6d5336a8edbc74dff4
jma <masson.julien@outlook.be>
2025-10-06 15:20:31 +0200

    Update manifest & Implied group security

--- odashboard/__manifest__.py
+++ odashboard/__manifest__.py
@@ -12,6 +12,9 @@
         'web',
         'mail',
     ],
+    'external_dependencies': {
+        'python': ['PyPDF2'],
+    },
     'data': [
         # Security
         'security/odash_security.xml',
