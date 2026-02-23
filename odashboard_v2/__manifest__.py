f896cac36092fcf994182a86d5e3785d1526ad63
jma <masson.julien@outlook.be>
2026-02-23 16:31:16 +0100

    update sources

--- odashboard_v2/__manifest__.py
+++ odashboard_v2/__manifest__.py
@@ -25,9 +25,9 @@
     ],
     'assets': {
         'web.assets_backend': [
-            'odashboard_v2static/src/css/odashboard_action.css',
-            'odashboard_v2static/src/js/odashboard_action.js',
-            'odashboard_v2static/src/xml/odashboard_action.xml',
+            'odashboard_v2/static/src/css/odashboard_action.css',
+            'odashboard_v2/static/src/js/odashboard_action.js',
+            'odashboard_v2/static/src/xml/odashboard_action.xml',
         ],
     },
     'license': 'LGPL-3',
