a4e55e8fbff4624dc1a55b0f76745cedd3c136fd
jma <masson.julien@outlook.be>
2026-02-25 20:50:37 +0100

    fix v16 strucutre

--- odashboard_v2/__manifest__.py
+++ odashboard_v2/__manifest__.py
@@ -27,6 +27,8 @@
         'web.assets_backend': [
             'odashboard_v2/static/src/css/odashboard_action.css',
             'odashboard_v2/static/src/js/odashboard_action.js',
+        ],
+        'web.assets_qweb': [
             'odashboard_v2/static/src/xml/odashboard_action.xml',
         ],
     },
